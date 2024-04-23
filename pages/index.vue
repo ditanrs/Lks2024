07cb04920257b1b09e49036f3fb4eddd929fc348
ditanrs <ditanursabila@gmail.com>
2024-04-23 21:36:58 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <h4 class="text-center mt-4">Selamat Datang di APOTEK XYZ</h4>
-    <h5 class="text-center mt-4">Anda adalah..</h5>
+    <h5 class="text-center mt-4">Anda adalah...</h5>
   </div>
 </template>
